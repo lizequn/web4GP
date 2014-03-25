67f69abb952a2ca61237b489248a0556328641ff
lizequn <lzq910123@gmail.com>
2014-03-25 18:38:57 +0000

    a

--- src/main/java/UploadServlet.java
+++ src/main/java/UploadServlet.java
@@ -11,7 +11,11 @@ import org.apache.commons.fileupload.FileItem;
 import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.output.*;
+import uk.ac.ncl.cs.group1.clientapi.DocSender;
+import uk.ac.ncl.cs.group1.clientapi.core.DocSenderImpl;
+import uk.ac.ncl.cs.group1.clientapi.core.KeyPairStore;
 
 public class UploadServlet extends HttpServlet {
 
@@ -22,78 +26,48 @@ public class UploadServlet extends HttpServlet {
     private File file ;
 
     public void init( ){
-        // Get the file location where it would be stored.
-        filePath =
-                getServletContext().getInitParameter("file-upload");
+
     }
     public void doPost(HttpServletRequest request,
                        HttpServletResponse response)
             throws ServletException, java.io.IOException {
-        // Check that we have a file upload request
-        isMultipart = ServletFileUpload.isMultipartContent(request);
-        response.setContentType("text/html");
-        java.io.PrintWriter out = response.getWriter( );
-        if( !isMultipart ){
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet upload</title>");
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<p>No file uploaded</p>");
-            out.println("</body>");
-            out.println("</html>");
-            return;
+        if(request.getSession().getAttribute("key") == null){
+            response.sendRedirect("/error.jsp");
         }
-        DiskFileItemFactory factory = new DiskFileItemFactory();
-        // maximum size that will be stored in memory
-        factory.setSizeThreshold(maxMemSize);
-        // Location to save data that is larger than maxMemSize.
-        factory.setRepository(new File("c:\\temp"));
+        // Check that we have a file upload request
+        try {
+            List<FileItem> items = new ServletFileUpload(new DiskFileItemFactory()).parseRequest(request);
+            File file1 = null;
+            for (FileItem item : items) {
+                if (item.isFormField()) {
+                } else {
+                    String fieldname = item.getFieldName();
+                    String filename = FilenameUtils.getName(item.getName());
+                    System.out.println(filename);
+                    InputStream filecontent = item.getInputStream();
+                    OutputStream outputStream =
+                            new FileOutputStream(new File(filename));
 
-        // Create a new file upload handler
-        ServletFileUpload upload = new ServletFileUpload(factory);
-        // maximum file size to be uploaded.
-        upload.setSizeMax( maxFileSize );
+                    int read = 0;
+                    byte[] bytes = new byte[1024];
 
-        try{
-            // Parse the request to get file items.
-            List fileItems = upload.parseRequest(request);
-
-            // Process the uploaded file items
-            Iterator i = fileItems.iterator();
-
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet upload</title>");
-            out.println("</head>");
-            out.println("<body>");
-            while ( i.hasNext () )
-            {
-                FileItem fi = (FileItem)i.next();
-                if ( !fi.isFormField () )
-                {
-                    // Get the uploaded file parameters
-                    String fieldName = fi.getFieldName();
-                    String fileName = fi.getName();
-                    String contentType = fi.getContentType();
-                    boolean isInMemory = fi.isInMemory();
-                    long sizeInBytes = fi.getSize();
-                    // Write the file
-                    if( fileName.lastIndexOf("\\") >= 0 ){
-                        file = new File( filePath +
-                                fileName.substring( fileName.lastIndexOf("\\"))) ;
-                    }else{
-                        file = new File( filePath +
-                                fileName.substring(fileName.lastIndexOf("\\")+1)) ;
+                    while ((read = filecontent.read(bytes)) != -1) {
+                        outputStream.write(bytes, 0, read);
                     }
-                    fi.write( file ) ;
-                    out.println("Uploaded Filename: " + fileName + "<br>");
+                    file1 = new File(filename);
                 }
             }
-            out.println("</body>");
-            out.println("</html>");
-        }catch(Exception ex) {
-            System.out.println(ex);
+            System.out.println(file1.length());
+            KeyPairStore keyPairStore = (KeyPairStore) request.getSession().getAttribute("key");
+            String toAddress = (String) request.getSession().getAttribute("toAddress");
+            List<UUID> list = new ArrayList<>();
+            DocSender sender = new DocSenderImpl(keyPairStore);
+            UUID uuid = sender.sendDoc(file1,toAddress,true);
+            list.add(uuid);
+            request.getSession().setAttribute("uuid",list);
+            response.sendRedirect("getreceipt.jsp");
+        } catch (FileUploadException e) {
+            throw new ServletException("Cannot parse multipart request.", e);
         }
     }
     public void doGet(HttpServletRequest request,
--- src/main/webapp/uploadfile.jsp
+++ src/main/webapp/uploadfile.jsp
@@ -12,14 +12,14 @@
     <title>File Uploading Form</title>
 </head>
 <body>
+
 <h3>File Upload:</h3>
 Select a file to upload: <br />
 <form action="UploadServlet" method="post"
       enctype="multipart/form-data">
     <input type="file" name="file" size="50" />
     <br />
-    <input type="text" name="email">
-    <br/>
+
     <input type="submit" value="Upload File" />
 </form>
 </body>
